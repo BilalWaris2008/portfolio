7c494830c723d4042601b45448d1cbdbde74bf60
M.Bilal Waris <bilalwaris543@gmail.com>
2026-01-16 23:06:28 +0500

    Added Functionality on Clicking Portfolio Lists

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const resumeLists = document.querySelectorAll('.resume-list');
 const resumeBoxs = document.querySelectorAll('.resume-box');
 
-// resume section when clicking tab lists
+const portfolioLists = document.querySelectorAll('.portfolio-list');
+
+// resume section when clicking tab list
 resumeLists.forEach((list, idx) => {
     list.addEventListener('click', () => {
         document.querySelector('.resume-list.active').classList.remove('active');
         list.classList.add('active');
         document.querySelector('.resume-box.active').classList.remove('active');
         resumeBoxs[idx].classList.add('active');
+    })
+});
+
+// portfolio section when clicking tab list
+portfolioLists.forEach((list, idx) => {
+    list.addEventListener('click', () => {
+        document.querySelector('.portfolio-list.active').classList.remove('active');
+        list.classList.add('active');
     })
 });
